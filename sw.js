78bd96a217012aabf970c880d29e6f27711c4cba
Africavision140 <visionafriaca@gmail.com>
2026-09-24 11:14:34 +0000

    Update service worker version to livrapro-v11

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    LivraPro — service worker
    À INCRÉMENTER À CHAQUE MODIFICATION DE index.html :
    ========================================================================== */
-const VERSION = 'livrapro-v9';
+const VERSION = 'livrapro-v11';
 
 /* Fichiers mis en cache dès l'installation. */
 const SHELL = [
